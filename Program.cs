ebb4a92c3c36db2622d2f6ee96625942ae7f0542
Anildo Caldas <anildk5@hotmail.com>
2020-06-17 12:36:06 -0300

    Added iis and kestrel configuration to publishe api correctly on IIS

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace netcore3_api_basicproject
 {
@@ -20,6 +15,9 @@ namespace netcore3_api_basicproject
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseKestrel();
+                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseIISIntegration();
                     webBuilder.UseStartup<Startup>();
                 });
     }
